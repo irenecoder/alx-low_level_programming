8b284f2c714f98631ada3aca4521068a6801a78b
irenecoder <wairene60@gmail.com>
2023-03-20 18:58:48 -0700

    Debug 4-puts.c2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#!/bin/bash
-/* a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
 #include <stdio.h>
 
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
