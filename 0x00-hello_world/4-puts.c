1adebdf0e0e511f2c35297d3e35d9d9f7651be69
irenecoder <wairene60@gmail.com>
2023-03-20 18:52:37 -0700

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/* a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
+#include<stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+}
