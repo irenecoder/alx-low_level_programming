85ef7820af6f9aa7a484bce2c899b14493cb0a40
irenecoder <wairene60@gmail.com>
2023-03-22 19:03:44 -0700

    Debug 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+#include <string.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main()
+* main - Entry point
+* Description: 'functions and nested loops'
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	putchar("_putchar\n");
-	return(0);
+	char printed_string[] = "_putchar";
+	int counter = 0;
+
+	while (counter <= 8)
+	{
+		_putchar(printed_string[counter]);
+		counter++;
+	}
+	return (0);
 }
